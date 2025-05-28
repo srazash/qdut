8116d916d84f366c4ac80cfedc415ba60b312d8b
Ryan Shaw-Harrison <srazash@protonmail.com>
2025-05-28 19:25:34 +0100

    GetDotnetDirs() returns a map of dirs and isValidPath() check if the paths are valid.

--- cmd/dirs.go
+++ cmd/dirs.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -12,20 +13,38 @@ const DOTNET_RUNTIME = "/shared/Microsoft.NETCore.App"
 const ASPNET_RUNTIME = "/shared/Microsoft.AspNetCore.App"
 
 func getDotnetBaseDir() string {
-	basePath, err := exec.LookPath("dotnet")
+	baseDir, err := exec.LookPath("dotnet")
 	if err != nil {
-		log.Fatalf("failed to find dotnet path: %s\n", err)
+		fmt.Printf("Unable to detect dotnet in the $PATH,")
+		fmt.Printf("try passing in the path using the -p flag")
+		log.Fatalf("%s\n", err)
 	}
-	return basePath
+	return strings.TrimSuffix(baseDir, "/dotnet")
+}
+
+func isValidPDir(dir string) bool {
+	_, err := os.Stat(dir)
+	return err == nil
+}
+
+func GetDotnetDirs() map[string]string {
+	dotnetDirs := make(map[string]string, 3)
+
+	dotnetBaseDir := getDotnetBaseDir()
+
+	dotnetDirs["sdk"] = dotnetBaseDir + DOTNET_SDK
+	dotnetDirs["dotnet_runtime"] = dotnetBaseDir + DOTNET_RUNTIME
+	dotnetDirs["aspnet_runtime"] = dotnetBaseDir + ASPNET_RUNTIME
+
+	return dotnetDirs
 }
 
 func PrintDotnetDirs() {
-	dotnetDir := getDotnetBaseDir()
+	dotnetDirs := GetDotnetDirs()
 
-	dotnetDir = strings.TrimSuffix(dotnetDir, "/dotnet")
+	fmt.Printf("dotnet base directory found in PATH: %s\n", getDotnetBaseDir())
 
-	fmt.Printf("dotnet base directory: %s\n", dotnetDir)
-	fmt.Printf("\tdotnet sdk directory: %s\n", dotnetDir+DOTNET_SDK)
-	fmt.Printf("\tdotnet runtime directory: %s\n", dotnetDir+DOTNET_RUNTIME)
-	fmt.Printf("\taspnet runtime directory: %s\n", dotnetDir+ASPNET_RUNTIME)
+	fmt.Printf("\tdotnet sdk directory: %s ? %t\n", dotnetDirs["sdk"], isValidPDir(dotnetDirs["sdk"]))
+	fmt.Printf("\tdotnet runtime directory: %s ? %t\n", dotnetDirs["dotnet_runtime"], isValidPDir(dotnetDirs["dotnet_runtime"]))
+	fmt.Printf("\taspnet runtime directory: %s ? %t\n", dotnetDirs["aspnet_runtime"], isValidPDir(dotnetDirs["aspnet_runtime"]))
 }
